6c1944f9f80157d62915cedf4804448f2f7f6cd3
taijuber808 <taijuber808@gmail.com>
2025-12-17 16:29:07 +0530

    Add files via upload

---
+++ Day 12   Pattern/Q2.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+int main (){
+
+//j = row
+//i = colum
+
+
+    int j , i;
+
+
+for ( j = 4 ; j >= 1 ; j--)
+{
+    for ( i = 1 ; i <= j; i++)
+    {
+        printf("* ");        
+
+    } 
+    printf ("\n");          
+    
+}
+
+
+
+    return 0;
+}
+
+
+
+//*  * *  *
+
+//*  * *  
+//*  *
+//*  
---
+++ Day 12   Pattern/Q3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main (){
+
+//j = row
+//i = colum
+    int j , i;
+
+
+for ( j = 1 ; j <= 4 ; j++)
+{
+    for ( i = 1; i <= j; i++)
+    {
+        printf("%d ",i);
+    } 
+    printf ("\n");
+    
+}
+
+
+
+    return 0;
+}
