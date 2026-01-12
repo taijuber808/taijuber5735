33e855dd8eae5496550fda07581947a37514fe98
taijuber808 <taijuber808@gmail.com>
2026-01-12 15:00:47 +0530

    Add files via upload

---
+++ JS   Project Day 1/script.js
@@ -0,0 +1,34 @@
+function plus(){
+    let a=10 , b=20
+    console.log(`adition is ${a+b}`);
+    
+}
+
+function sub (){
+    let a = 40,b=30
+    console.log(`sub is  ${a-b}`);
+    
+}
+
+function mul (){
+    let a = 30 , b=20
+    console.log(`multipication is ${a*b}`);
+    
+}
+
+function div (){
+    let a=100, b=20
+    console.log(`division is ${a/b}`);
+    
+}
+function mod(){
+    let a = 10, b= 20
+    console.log(`moduleus is ${a%b}`);
+    
+}
+
+plus()
+sub()
+mul()
+div()
+mod()
