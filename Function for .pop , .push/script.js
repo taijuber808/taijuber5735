4080405481b39def1a0788e4a48094a975524d1e
taijuber808 <taijuber808@gmail.com>
2026-01-22 16:14:41 +0530

    Add files via upload

---
+++ Function for .pop , .push/script.js
@@ -0,0 +1,45 @@
+// push function array
+
+let fruits = ["Apple", "Banana"];
+
+fruits.push("Mango");
+
+console.log(fruits);
+
+
+
+// pop function array
+
+let colors = ["Red", "Green", "Blue"];
+
+colors.pop();
+
+console.log(colors);
+
+
+//shift function array
+
+let cities = ["Delhi", "Mumbai", "Pune"];
+
+cities.shift();
+
+console.log(cities);
+
+
+
+//unshift function array
+
+let numbers = [20, 30, 40];
+
+numbers.unshift(10);
+
+console.log(numbers);
+
+// splice function array
+
+
+let marks = [50, 60, 70, 80];
+
+marks.splice(1, 2);
+
+console.log(marks);
