25fc16a736e4dbfda21a98fc2c364952f57018c8
taijuber808 <taijuber808@gmail.com>
2026-02-07 13:47:32 +0530

    Add files via upload

---
+++ Count Down  Project/script.js
@@ -0,0 +1,10 @@
+     function updateDateTime() {
+      const now = new Date();
+      const date = now.toLocaleDateString();"<br>"
+      const time = now.toLocaleTimeString();
+
+      document.getElementById("datetime").innerHTML = `${date} ${time}`;
+      }
+      
+      setInterval(updateDateTime, 1000); 
+      updateDateTime();
